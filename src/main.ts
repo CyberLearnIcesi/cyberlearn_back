428bbc08ee4eee63481171fa29585f5f5a3f8451
dylanbc1 <dyez1110@gmail.com>
2025-02-26 10:21:20 -0500

    feat: Add global routes prefix

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import "reflect-metadata";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // prefijo global para todas las rutas
+  app.setGlobalPrefix('cyberlearn');
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
